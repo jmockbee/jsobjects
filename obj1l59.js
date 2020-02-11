01f6c053b2579e7baf9a9f22f164d6458e624be8
Jon Mockbee <mockbee1000@gmail.com>
2020-02-10 20:13:51 -0500

    adding to empty object

---
+++ obj1l59.js
@@ -0,0 +1,7 @@
+const fitBitData = {
+	totalSteps: 308727,
+	totalMiles: 211.7,
+	averageCalorieBurn: 5755,
+	workoutsThisWeek: '5 of 7',
+	averageGoodSleep: '2:13'
+};
---
+++ obj1l60.js
@@ -0,0 +1,6 @@
+
+const palette = {
+    red: '#eb4d4b',
+    yellow: '#f9ca24',
+    blue: '#30336b'
+};
